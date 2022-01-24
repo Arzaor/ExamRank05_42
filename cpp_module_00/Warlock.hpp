a7c0cfda96e80be3b553b62192587eceb6d75b98
BARETTE <zyrtec06200@live.fr>
2022-01-24 13:59:21 +0100

    First module complete

---
+++ cpp_module_00/Warlock.hpp
@@ -0,0 +1,27 @@
+#ifndef WARLOCK_HPP
+# define WARLOCK_HPP
+
+# include <string>
+# include <iostream>
+
+class Warlock
+{
+	private:
+		std::string name;
+		std::string title;
+
+		Warlock();
+		Warlock(const Warlock &);
+		Warlock& operator=(const Warlock &);
+
+	public:
+		Warlock(const std::string &, const std::string &);
+		~Warlock();
+
+		const std::string&	getName() const;
+		const std::string&	getTitle() const;
+		void				setTitle(const std::string &);
+		void				introduce() const;
+};
+
+#endif
